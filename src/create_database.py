ba4d8744d31c9947c9a464d6a92d06450b22b873
LucBerge <lucas.bergeron@outlook.fr>
2019-04-01 20:49:10 +0200

    Add comments

--- src/create_database.py
+++ src/create_database.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+# USAGE
+
+# python create_database.py file_with_one_line_to_simulate_one_input.txt --database=test --query-limit=200000
+
 from __future__ import division
 import sys, os
 from mrjob.job import *
@@ -13,35 +17,34 @@ from utils.githubscrapper import *
 
 class CreateDatabase(MRJob):
 
-	# VARIABLES
+	# ATRIBUTS
 
-	kaggle = None
+	kaggle = None	#Big Query client for kaggle database
 
-	def configure_options(self):
-		super(CreateDatabase, self).configure_options()
+	def configure_options(self):	#Set options for command line call
+		super(CreateDatabase, self).configure_options()	#Call the super constructor
 
-		self.add_passthrough_option(
+		self.add_passthrough_option(	#Add option --current-folder wich represent the current full path to the file
 			'--current-folder', dest='current_folder', type='str',
 			help='The current folder')
-		self.add_passthrough_option(
+		self.add_passthrough_option(	#Add option --database wich is the name of the dataset we want to create
 			'--database', dest='database', type='str',
-			help='The database to use')
+			help='The name of the futur database')
 
-
-		self.add_passthrough_option(
+		self.add_passthrough_option(	#Add option --query-limit wich is the limit for one query (default 200000)
 			'--query-limit', dest='query_limit', default=200000, type='int',
 			help='The maximum number of rows in the query')
 
-	def steps(self):
+	def steps(self):	#Set steps
 		return [
-			MRStep(mapper_init=self.connect_to_kaggle,
+			MRStep(mapper_init=self.connect_to_kaggle,											#1 - Query the kaggle database
 					mapper=self.get_kaggle_mapper,
 					reducer=self.get_kaggle_reducer),
-			MRStep(mapper=self.scrapping),
-			MRStep(mapper=self.save)
+			MRStep(mapper=self.scrapping),														#2 - Scrap repos informations (currently desabled because commented)
+			MRStep(mapper_init=self.save_init, mapper=self.save, mapper_final=self.save_final)	#3 - Save the result
 		]
 
-	def connect_to_kaggle(self):
+	def connect_to_kaggle(self):	# Connect to kaggle
 		self.kaggle = bigquery.Client()
 
 	#########
@@ -56,21 +59,21 @@ class CreateDatabase(MRJob):
 				SELECT committer.email, committer.name, repo_name
 				FROM `bigquery-public-data.github_repos.commits`
 				WHERE RAND() < """ + str(self.options.query_limit) + """/(SELECT COUNT(*) FROM `bigquery-public-data.github_repos.commits`)
-				"""
+				"""	#Create the query which is a random selection from the commits table
 				
-			rows = self.kaggle.query(query)
+			rows = self.kaggle.query(query)	#Execute the query
 			self.log(str(self.options.query_limit) + " commits have been queried.")
 
-			for row in rows:
-				email = row['email']
-				name = row['name']
+			for row in rows:	#For each row in the result
+				email = row['email']	#Get the email
+				name = row['name']		#Get the name
 
-				if '@' in email and name:
-					yield {'email' : email, 'name' : name}, 1
+				if '@' in email and name:	#If the format is ok
+					yield {'email' : email, 'name' : name}, 1	#yield user as dict
 
-					for repo in row['repo_name']:
-						yield {'repo_name' : repo}, 1
-						yield {'repo_name' : repo, 'email' : email}, 1
+					for repo in row['repo_name']:				#For each repo
+						yield {'repo_name' : repo}, 1					#yield the repo as dict
+						yield {'repo_name' : repo, 'email' : email}, 1	#yield the link as dict
 
 			self.log(str(self.options.query_limit) + " commits have been saved.")
 
@@ -78,42 +81,53 @@ class CreateDatabase(MRJob):
 			self.log("Maximum quota reached.")
 
 	def get_kaggle_reducer(self, key, values):
-		yield key, sum(values)
+		yield key, sum(values)	# Sum the number of commits from one user on one repo
 
 	#STEP 2 = Scrap the repos and users (Optional)
 	def scrapping(self, key, value):
-		if 'repo_name' in key and 'email' in key:
-			yield key, value
-		elif 'email' in key:
-			#us = UserScrapper(key['email'])
-			#us.scrap()
-			yield key, value
-		elif 'repo_name' in key:
-			try:
-				rs = RepoScrapper(key['repo_name'])
-				#rs.scrap(True)
-				key['commits'] = rs.getCommits()
-				key['branches'] = rs.getBranches()
-				key['releases'] = rs.getReleases()
-				key['contributors'] = rs.getContributors()
-				key['issues'] = rs.getIssues()
-				key['pull_requests'] = rs.getPullRequests()
-				key['watchs'] = rs.getWatchs()
-				key['stars'] = rs.getStars()
-				key['forks'] = rs.getForks()
-				yield key, value
-			except CannotScrapRepoException as e:
-				self.log(str(e))
+		if 'repo_name' in key and 'email' in key:	#If it is a link
+			yield key, value 							#do nothing and yield
+		elif 'email' in key:						#If it is a user
+			#us = UserScrapper(key['email'])			#Create the scrap object
+			#us.scrap()									#Scrap
+														#Get results
+			yield key, value 							#yield the user
+		elif 'repo_name' in key:					#If it is a repo
+			try:										#Try to scrap
+				rs = RepoScrapper(key['repo_name'])			#Create the scrap object
+				#rs.scrap(True)								#Scrap
+				key['commits'] = rs.getCommits()			#Store the numbers of commits in the key
+				key['branches'] = rs.getBranches()			#Store the numbers of branches in the key
+				key['releases'] = rs.getReleases()			#Store the numbers of releases in the key
+				key['contributors'] = rs.getContributors()	#Store the numbers of contributors in the key
+				key['issues'] = rs.getIssues()				#Store the numbers of issues in the key
+				key['pull_requests'] = rs.getPullRequests()	#Store the numbers of pull_requests in the key
+				key['watchs'] = rs.getWatchs()				#Store the numbers of watchs in the key
+				key['stars'] = rs.getStars()				#Store the numbers of stars in the key
+				key['forks'] = rs.getForks()				#Store the numbers of forks in the key
+				yield key, value  							#yield the repo
+			except CannotScrapRepoException as e:		#If impossible to scrap the repo
+				self.log(str(e))							#Display why and do not yield it
 
 	#STEP 3 = Save repos and users
+	def save_init(self):											#Called once before the save mapper
+		self.repos = open(self.options.database + "_repos.txt", "w")	#Open or create the repos file
+		self.users = open(self.options.database + "_users.txt", "w")	#Open or create the users file
+		self.links = open(self.options.database + "_links.txt", "w")	#Open or create the links file
+
 	def save(self, key, value):
-		if 'repo_name' in key and 'email' in key:
-			print(key['repo_name'] + '\t' + key['email'] + '\t' + str(value) + '\n')
-		elif 'email' in key:
-			pass #Do nothing for repos (for the moment)
-		elif 'repo_name' in key:
-			pass #Do nothing for the users (for the moment)
+		if 'repo_name' in key and 'email' in key:									#If it is a link
+			self.links.write(key['repo_name'] + '\t' + key['email'] + '\t' + str(value) + '\n')		#yield it as string
+		elif 'email' in key:														#If it is a user
+			self.users.write(key['email'] + '\t' + key['name']) 						#Do nothing for repos (for the moment)
+		elif 'repo_name' in key:													#If it is a repo
+			self.repos.write(key['repo_name'] + '\t' + str(key['commits']) + '\t' + str(key['branches']) + '\t' + str(key['releases']) + '\t' + str(key['contributors']) + '\t' + str(key['issues']) + '\t' + str(key['pull_requests']) + '\t' + str(key['watchs']) + '\t' + str(key['stars']) + '\t' + str(key['forks'])) #Do nothing for the users (for the moment)
 			
+	def save_final(self):	#Called once after the save mapper
+		self.repos.close()		#Close the repos file
+		self.users.close()		#Close the users file
+		self.links.close()		#Close the links file
+
 	#######
 	# LOG #
 	#######
@@ -128,24 +142,24 @@ class CreateDatabase(MRJob):
 # MAIN #
 ########
 
-if __name__ == '__main__':
+if __name__ == '__main__':	#Main function
 	try:
 		valid = False
-		for arg in sys.argv:
-			if('--database=' in arg):
-				valid = True
-				sys.argv.append('--current-folder=' + os.getcwd() + '/')
-				CreateDatabase.run()
+		for arg in sys.argv:			#For each argument
+			if('--database=' in arg):		#If --database option is set
+				valid = True 					#Command line valid
+				sys.argv.append('--current-folder=' + os.getcwd() + '/')	#Add the current path
+				CreateDatabase.run()										#Run mrJob
 
-		if(not valid):
-			print("You forgot the --database argument.")
+		if(not valid):	#If no valid
+			print("You forgot the --database argument. DO NOT RUN WITH HADOOP !")	#Explain why
 
-	except DefaultCredentialsError:
+	except DefaultCredentialsError:	#If trouble with GOOGLE_APPLICATION_CREDENTIALS variable
 		print('You first have to set GOOGLE_APPLICATION_CREDENTIALS environnement variable.')
-		print('Open a console and type "export GOOGLE_APPLICATION_CREDENTIALS=path/to/google/credentials.json".')
+		print('Open a console and type "export GOOGLE_APPLICATION_CREDENTIALS=/full/path/to/google/credentials.json".')
 
-	except KeyboardInterrupt:
-		pass
+	except KeyboardInterrupt:		#If Ctrl+C
+		pass 							#Do nothing
 
-	except:
-		print(traceback.print_exc())
+	except:							#For any other exception
+		print(traceback.print_exc())	#print the stack trace
--- src/pagerank.py
+++ src/pagerank.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+# USAGE
+
+# python pagerank.py input.txt --iterations=10 --damping-factor=0.85
+# python pagerank.py -r hadoop hdfs:///folders/input.txt --output-dir hdfs:///folders/output --no-output --iterations=10 --damping-factor=0.85
+
 from __future__ import division
 import sys, os, traceback, ast
 from mrjob.job import *
@@ -7,30 +12,25 @@ from utils import githubdatabase
 
 class PageRank(MRJob):
 
-	# VARIABLES
-
-	db = None
-	counter = 0
-
-	def configure_options(self):
-		super(PageRank, self).configure_options()
+	def configure_options(self):	#Set options for command line call
+		super(PageRank, self).configure_options()	#Call the super constructor
 		
-		self.add_passthrough_option(
+		self.add_passthrough_option(	#Add option --iterations wich represent the number of iterations for the pagerank (default 10)
 			'--iterations', dest='iterations', default=10, type='int',
 			help='The number of iterations')
 		self.add_passthrough_option(
-			'--damping-factor', dest='damping_factor', default=0.85, type='float',
+			'--damping-factor', dest='damping_factor', default=0.85, type='float',	#Add option --damping-factor wich represent the pourcentage of weigth the node keep for himself (default 0.85)
 			help='Pourcentage of weigth the node keep for himself')
 
-	def steps(self):
-		steps = [MRStep(mapper=self.get_commits_mapper, reducer=self.get_commits_reducer)]
-		steps.append(MRStep(reducer=self.get_weight_reducer))
+	def steps(self):	#Set steps
+		steps = [MRStep(mapper=self.get_commits_mapper, reducer=self.get_commits_reducer)]	#1 - Get commits
+		steps.append(MRStep(reducer=self.get_weight_reducer))								# 2 - Get the default waight
 
 		for i in range(self.options.iterations):
-			steps.append(MRStep(mapper=self.pagerank_mapper,
+			steps.append(MRStep(mapper=self.pagerank_mapper,								# 3 to N - Page rank step
 								reducer=self.pagerank_reducer))
 
-		steps.append(MRStep(mapper=self.output_mapper, reducer=self.output_reducer))
+		steps.append(MRStep(mapper=self.output_mapper, reducer=self.output_reducer))		# 4 - Sort the output
 
 		return steps
 
@@ -43,36 +43,35 @@ class PageRank(MRJob):
 	# Get all links
 	def get_commits_mapper(self, key, value):
 
-		values = value.strip().split('\t')
+		values = value.strip().split('\t')	 #Strop and split each line
 
-		if len(values) == 3:
-			repo = values[0]
-			email = values[1]
-			commits = values[2]
+		if len(values) == 3:				#If the line have 3 values
+			repo = values[0]					# Get the repo name
+			email = values[1]					# Get the email
+			commits = values[2]					#Get the number of commits
 
-			yield {'key' : repo}, (email, int(commits))
-			yield {'key' : email}, (repo, int(commits))
+			yield {'key' : repo}, (email, int(commits))		#Yield the repo node as dict
+			yield {'key' : email}, (repo, int(commits))		#Yield the user node as dict
 
 	# Get all nodes with there links
 
-	def get_commits_reducer(self, node, values):
+	def get_commits_reducer(self, node, values):	#Calcul the pourcentage for each link
 		links = {}
-		tab_values = [value for value in values]
-		total_commits = sum([value[1] for value in tab_values])
+		tab_values = [value for value in values]	#Get the values as a regular list
+		total_commits = sum([value[1] for value in tab_values])	#Get the total number of commits for the node
 
-		for value in tab_values:
-			links[value[0]] = value[1]/total_commits
+		for value in tab_values:						#For each connected node
+			links[value[0]] = value[1]/total_commits		#Add the pourcentage for the node in the link dict
 
-		node['links'] = links
-		yield None, node
+		node['links'] = links	#Add the links dict to the key
+		yield None, node 		#Yield all nodes with the None key to be able to acces all nodes from one key
 
 	# STEP 3 = Get the default weight for each node
-
-	def get_weight_reducer(self, _, nodes):
-		tab_nodes = [node for node in nodes]
-		N = len(tab_nodes)
-		for node in tab_nodes:
-			yield node, 1/N
+	def get_weight_reducer(self, _, nodes):	
+		tab_nodes = [node for node in nodes] 	#Get the values as a regular list
+		N = len(tab_nodes)						#Get the number of nodes
+		for node in tab_nodes:					#For each node
+			yield node, 1/N 						#Yield the node as key with the default weight as value
 
 	#STEP 4 to 4+N = Page rank
 	def pagerank_mapper(self, node, weight):
@@ -95,23 +94,20 @@ class PageRank(MRJob):
 
 	#STEp 5+N = Sort output
 	def output_mapper(self, node, weight):
-		yield None, (weight, node)
+		yield None, (weight, node)			#Yield all nodes with the None key to be able to acces all nodes from one key
 
 	def output_reducer(self, _, nodes):
-		for weight, node in sorted(nodes, reverse=True):
-			yield (weight, node['key'])
+		for weight, node in sorted(nodes, reverse=True):	#For all nodes sorted by weight
+			yield (weight, node['key'])							#Yield the final weight
 
 ########
 # MAIN #
 ########
 
-if __name__ == '__main__':
-	try:
-		PageRank.run()
-	except KeyboardInterrupt:
-		pass
-	except:
-		print(traceback.print_exc())
-
-# python pagerank.py -r local /folders/input.txt --output-dir hdfs:///folders/output --no-output --iterations=10 --damping-factor=0.85
-# python pagerank.py -r hadoop hdfs:///folders/input.txt --output-dir hdfs:///folders/output --no-output --iterations=10 --damping-factor=0.85
+if __name__ == '__main__':		#If main function
+	try:							#Try
+		PageRank.run()					#Run mrJob
+	except KeyboardInterrupt:		#If Ctrl+C
+		pass 							#Do nothing
+	except:							#If any other exception
+		print(traceback.print_exc())	#print it
