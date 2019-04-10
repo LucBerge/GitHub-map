19933959353ef37590d68768b4554f2fdb8776d9
LucBerge <lucas.bergeron@outlook.fr>
2019-04-10 18:16:35 +0200

    Update

--- src/tests/bigqueryTest.py
+++ src/tests/bigqueryTest.py
@@ -1,54 +1,67 @@
 #!/usr/bin/python
 
-import os, traceback
+import traceback
 from google.cloud import bigquery
-from google.auth.exceptions import DefaultCredentialsError
 
-def estimation():
-	LIMIT = 1
-	OFFSET = 0
-	QUERY = """
-			SELECT committer.email, committer.name, repo_name
-			FROM `bigquery-public-data.github_repos.commits`
-			LIMIT """ + str(LIMIT) + """
-			OFFSET """ + str(OFFSET)
-
+def estimation(QUERY):
 	client = bigquery.Client()
 	job_config = bigquery.QueryJobConfig()
 	job_config.dry_run = True
 	job_config.use_query_cache = False
 	query_job = client.query((QUERY), job_config=job_config)
-
 	assert query_job.state == 'DONE'
 	assert query_job.dry_run
-
 	print("This query will process {} bytes.".format(query_job.total_bytes_processed))
 
-def query():
+def query(QUERY):
 	service = bigquery.Client()
+	rows = service.query(QUERY)
 
-	QUERY = """
-	SELECT C.committer.email, C.committer.name, R.repo_name
-	FROM `bigquery-public-data.github_repos.commits` C, `bigquery-public-data.github_repos.sample_repos` R
-	WHERE R.repo_name IN UNNEST(C.repo_name)
-	LIMIT 10
-	OFFSET 0
-	"""
-
-	QUERY = """
-	SELECT committer.email, committer.name, repo_name
-	FROM `bigquery-public-data.github_repos.commits`
-	WHERE RAND() < 1000000/(SELECT COUNT(*) FROM `bigquery-public-data.github_repos.commits`)
-	"""
-
-	results = service.query(QUERY)
-
-	for row in results:
+	counter = 0
+	for row in rows:
 		print(row)
+		counter += 1
+		if (counter == 100):
+			break
+
+def main():
+	QUERY = """
+		SELECT committer.email, committer.name, repo_name
+		FROM `bigquery-public-data.github_repos.commits`
+		WHERE RAND() < 1000000/(SELECT COUNT(*) FROM `bigquery-public-data.github_repos.commits`)
+		"""
+
+	QUERY = """
+		WITH links AS
+			(SELECT committer.email, repo_name
+			FROM `bigquery-public-data.github_repos.commits`)
+		SELECT email, repo, COUNT(email) AS commits
+		FROM links
+		CROSS JOIN UNNEST(repo_name) AS repo
+		GROUP BY email, repo
+		#HAVING COUNT(email) > 100
+		"""
+
+	QUERY = """
+		WITH test AS
+			(WITH links AS
+				(SELECT committer.email, repo_name
+				FROM `bigquery-public-data.github_repos.commits`)
+			SELECT COUNT(*)
+			FROM links
+			CROSS JOIN UNNEST(repo_name) AS repo
+			GROUP BY email, repo)
+		SELECT COUNT(*)
+		FROM test
+		"""
+
+	#query(QUERY)
+	estimation(QUERY)
+
 
 if __name__ == "__main__":
 	try:
-		estimation()
+		main()
 	except KeyboardInterrupt:
 		pass
 	except:
