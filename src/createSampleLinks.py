f5316274fe3ef03aa3e8f192dcb567f8d376f314
LucBerge <lucas.bergeron@outlook.fr>
2019-04-18 16:20:50 +0200

    Add limit option

--- src/createSampleLinks.py
+++ src/createSampleLinks.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python
 
-import sys, os
+import sys
 
-exists1 = os.path.isfile(sys.argv[1])
-if(not exists1):
-	print("Usage <input> <output>")
-	sys.exit(-1)
-	
-input = open(sys.argv[1],"rU")
-output = open(sys.argv[2],"w")
-
-for line in input:
-	line_split = line.split('\t')
-	if(len(line_split) == 3):
-		if(int(line_split[2]) > 50):
-			output.write(line)
-
-input.close()
-output.close()
+try:		
+	input = open(sys.argv[1],"rU")
+	output = open(sys.argv[2],"w")
+	limit = int(sys.argv[3])
+	for line in input:
+		line_split = line.split('\t')
+		if(len(line_split) == 3):
+			if(int(line_split[2]) >= limit):
+				output.write(line)
+	input.close()
+	output.close()
+except:
+	print("Usage <input> <output> <limit>")
